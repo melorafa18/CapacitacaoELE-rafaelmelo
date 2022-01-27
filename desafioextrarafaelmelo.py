afe4b5aeac92126f82e74a938be3d66174014376
melorafa18 <93156012+melorafa18@users.noreply.github.com>
2022-01-27 14:53:24 -0300

    Update desafioextrarafaelmelo.py

--- desafioextrarafaelmelo.py
+++ desafioextrarafaelmelo.py
@@ -2,7 +2,6 @@ def verifica():
     '''Compara o preço de compra com os de pratileira e verifica se estão
     corretor'''
     preco_prd={"arduino":74,"servoMotor":5,"potenciomentro":1.9,"LEDverm":1,"LEDverd":1,"resistor1":0.1,"resistor100k":0.5}
-    #print(preco_prd.get("arduino"))
     comprados=["resistor100k","resistor100k","servoMotor","arduino","arduino","potenciomentro","resistor1","LEDverm","LEDverm","LEDverm","LEDverd","LEDverd"]
     precoPago = [0.7,1,5,100,74,2.5,0.1,1,1,2,2,1]
     erro=0
